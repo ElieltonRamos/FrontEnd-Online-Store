bb2663f6880ee740bd82f19718a3f681b396490f
ElieltonRamos <elieltonramos14@gmail.com>
2023-07-11 01:02:29 -0300

    fix typo

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -10,7 +10,6 @@ import SearchList from '../components/SearchList';
 import Loading from '../components/Loading';
 import ShoppingCartIcon from '../components/ShoppingCartIcon';
 import * as Styled from '../Styles/HomePage.styles';
-import { Button } from '@mui/material';
 
 function HomePage({ itensCar, setItensCar }: PropsHomePage) {
   const [searchInput, setSearchInput] = useState('');
@@ -48,8 +47,13 @@ function HomePage({ itensCar, setItensCar }: PropsHomePage) {
             <Styled.ButtonSearch>
               <span>
                 <svg viewBox="0 0 24 24" height="24" width="24" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M9.145 18.29c-5.042 0-9.145-4.102-9.145-9.145s4.103-9.145 9.145-9.145 9.145 4.103 9.145 9.145-4.102 9.145-9.145 9.145zm0-15.167c-3.321 0-6.022 2.702-6.022 6.022s2.702 6.022 6.022 6.022 6.023-2.702 6.023-6.022-2.702-6.022-6.023-6.022zm9.263 12.443c-.817 1.176-1.852 2.188-3.046 2.981l5.452 5.453 3.014-3.013-5.42-5.421z">
-                  </path>
+                  <path
+                    d="M9.145 18.29c-5.042 0-9.145-4.102-9.145-9.145s4.103-9.145 9.14
+                    5-9.145 9.145 4.103 9.145 9.145-4.102 9.145-9.145 9.145zm0-15.167c-3.
+                    321 0-6.022 2.702-6.022 6.022s2.702 6.022 6.022 6.022 6.023-2.702 6.0
+                    23-6.022-2.702-6.022-6.023-6.022zm9.263 12.443c-.817 1.176-1.852 2.18
+                    8-3.046 2.981l5.452 5.453 3.014-3.013-5.42-5.421z"
+                  />
                 </svg>
               </span>
             </Styled.ButtonSearch>
